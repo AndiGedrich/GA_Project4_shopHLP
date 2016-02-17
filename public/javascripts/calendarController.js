49b1fce785574db40ea11301c10a74d35a1cc37c
Andrea Gedrich <andrea.d.amaro@gmail.com>
2016-02-17 15:42:17 -0800

    table dynamically generating

--- public/javascripts/calendarController.js
+++ public/javascripts/calendarController.js
@@ -14,13 +14,43 @@ angular.module('shopHLP')
     {}
     ];
 
+
     vm.createCalendar = function(){
       console.log(vm.date);
-      if (vm.date.indexOf("Jan" || "Mar" || "May" || "Jul" || "Aug" || "Oct" || "Dec")>= 1){
+      var today = vm.date;
+      var todayString = String(today);
+      if (todayString.indexOf("Feb")>-1){
+        console.log("got the date");
+        var generateRowsWithCols = function(rowStart, noOfRows, noOfCols){
+          if (rowStart> noOfRows)
+            return;
+          var table = document.getElementById("grid");
+          var tr = table.insertRow("tr");
+          tr.setAttribute("id", "tr"+rowStart);
+          generateCells(1, rowStart, noOfCols);
+          generateRowsWithCols(++rowStart, noOfRows,noOfCols);
+        };
 
+        var generateCells = function(colStart,rowStart,noOfCols){
+          if (colStart > noOfCols)
+            return;
+          var row = document.getElementById("tr"+rowStart);
+          var td = row.insertCell('td');
+          td.setAttribute("id", "td"+colStart);
+          generateCells(++colStart,rowStart,noOfCols);
+        };
 
+        var generateTable = function(noOfRows,noOfCols){
+          var table = document.createElement("table");
+          table.setAttribute("id", "grid");
+          document.body.appendChild(table);
+          generateRowsWithCols(1,noOfRows,noOfCols);
+        };
 
+      } else {
+        console.log("don't have the date");
       }
+      generateTable(5,6);
     }
-
+    vm.createCalendar();
   };
